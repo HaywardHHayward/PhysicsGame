230cb24d6dcf0f40c85ff44a4b456261db16952e
TGDiamond1 <tictanic8@hotmail.com>
2018-10-04 21:47:37 -0500

    Added acceleration functionality

--- Scripts/BaseObject.gd
+++ Scripts/BaseObject.gd
@@ -2,25 +2,28 @@ extends Area2D
 
 export(float, 299792458.0) var velocity = 0
 export(float) var mass = 0
+export(float, 299792458.0) var acceleration = 0
 
 const C = 299792458.0
+
+onready var baseMass = mass
 
 var is_showing = false
 
 func _ready():
 	$Overlay.hide()
 
-func lorentz(v):
+func lorentz_factor(v):
 	return sqrt(1.0-(pow(v, 2)/pow(C, 2)))
 
-func _process(delta):
-	_scale(velocity)
+func _physics_process(delta):
+	lorentz_transformation(velocity)
 	$Overlay/Container/VelocityContainer/Value.text = str(velocity)
 	$Overlay/Container/MassContainer/Value.text = str(mass)
-	velocity += C/600.0
+	$Overlay/Container/AccelerationContainer/Value.text = str(acceleration)
 	if is_showing:
 		$Overlay.rect_position = get_global_mouse_position()-position
-	
+	velocity += acceleration/60.0
 
 func _on_BaseObject_mouse_entered():
 	$Overlay.show()
@@ -30,6 +33,7 @@ func _on_BaseObject_mouse_exited():
 	$Overlay.hide()
 	is_showing = false
 	
-func _scale(v):
-	scale = Vector2(lorentz(v),1)
-	$Overlay.rect_scale = Vector2(1/lorentz(v), 1)
+func lorentz_transformation(v):
+	scale = Vector2(lorentz_factor(v),1)
+	$Overlay.rect_scale = Vector2(1/lorentz_factor(v), 1)
+	mass = baseMass/lorentz_factor(v)
