ee50c0f9c6c6404e5d4fe47531fed11961105bca
TGDiamond1 <tictanic8@hotmail.com>
2018-10-06 07:48:50 -0500

    Fixed merge problems

--- Scripts/BaseObject.gd
+++ Scripts/BaseObject.gd
@@ -3,11 +3,7 @@ extends Area2D
 export(float, 299792458.0) var velocity = 0
 export(float) var mass = 0
 export(float, 299792458.0) var acceleration = 0
-<<<<<<< HEAD
 export(float) var forceNewton
-=======
-export(float) var forceNewton = 0
->>>>>>> c23baeb54a81d1e3dded0446ee25f7e1810d5f20
 
 const C = 299792458.0
 
@@ -25,15 +21,11 @@ func _physics_process(delta):
 	if velocity >= C:
 		velocity = C - 0.01
 	lorentz_transformation(velocity)
-<<<<<<< HEAD
 	if forceNewton != null:
-		acceleration = forceNewton/mass
+		_applyForce(forceNewton)
 		$Overlay/Container/ForceContainer/Value.text = "%f N" % forceNewton
 	else:
 		$Overlay/Container/ForceContainer/Value.text = "N/A"
-=======
-	_applyForce(forceNewton)
->>>>>>> c23baeb54a81d1e3dded0446ee25f7e1810d5f20
 	$Overlay/Container/VelocityContainer/Value.text = "%f m/s" % velocity
 	$Overlay/Container/MassContainer/Value.text = "%f kg" % mass
 	$Overlay/Container/AccelerationContainer/Value.text = "%f m/s^2" % acceleration
