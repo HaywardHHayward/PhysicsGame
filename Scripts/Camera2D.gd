44fc2967c63de41dcde629197ab98d8ef55cea3d
TGDiamond1 <tictanic8@hotmail.com>
2018-11-28 18:29:44 -0600

    Added more indepth camera controls

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -11,19 +11,43 @@ func _process(delta):
 	if Input.is_action_pressed("ui_zoom_in"):
 		if Input.is_key_pressed(KEY_SHIFT):
 			cameraZoom /= 1.1
+		elif Input.is_key_pressed(KEY_CONTROL):
+			cameraZoom /= 1.001
 		else:
 			cameraZoom /= 1.01
 	elif Input.is_action_pressed("ui_zoom_out"):
 		if Input.is_key_pressed(KEY_SHIFT):
 			cameraZoom *= 1.1
+		elif Input.is_key_pressed(KEY_CONTROL):
+			cameraZoom *= 1.001
 		else:
 			cameraZoom *= 1.01
 	set_zoom(cameraZoom)
 	if Input.is_action_pressed("ui_up"):
-		offset.y -= 5*cameraZoom.y
+		if Input.is_key_pressed(KEY_SHIFT):
+			position.y -= 50*cameraZoom.y
+		elif Input.is_key_pressed(KEY_CONTROL):
+			position.y -= cameraZoom.y
+		else:
+			position.y -= 5*cameraZoom.y
 	elif Input.is_action_pressed("ui_down"):
-		offset.y += 5*cameraZoom.y
+		if Input.is_key_pressed(KEY_SHIFT):
+			position.y += 50*cameraZoom.y
+		elif Input.is_key_pressed(KEY_CONTROL):
+			position.y += cameraZoom.y
+		else:
+			position.y += 5*cameraZoom.y
 	if Input.is_action_pressed("ui_right"):
-		offset.x += 5*cameraZoom.x
+		if Input.is_key_pressed(KEY_SHIFT):
+			position.x += 50*cameraZoom.x
+		elif Input.is_key_pressed(KEY_CONTROL):
+			position.x += cameraZoom.x
+		else:
+			position.x += 5*cameraZoom.x
 	elif Input.is_action_pressed("ui_left"):
-		offset.x -= 5*cameraZoom.x
+		if Input.is_key_pressed(KEY_SHIFT):
+			position.x -= 50*cameraZoom.x
+		elif Input.is_key_pressed(KEY_CONTROL):
+			position.x -= cameraZoom.x
+		else:
+			position.x -= 5*cameraZoom.x
