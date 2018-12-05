2a8fcafeb79347fa627cd16988482ff85f68c455
TGDiamond1 <tictanic8@hotmail.com>
2018-12-05 15:28:20 -0600

    Fixes #1

--- Objects/MassLine.gd
+++
@@ -1,8 +0,0 @@
-extends LineEdit
-
-func _ready():
-	pass
-
-func _process(delta):
-	if float(text) > 10e1:
-		text = "%fe%d" % [float(text)/(floor(log(float(text))/log(10))), floor(log(float(text))/log(10))]
